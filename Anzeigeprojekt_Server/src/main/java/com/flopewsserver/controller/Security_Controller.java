c401ae5b4d01b0b0d242b65aef566c42c4ab076d
Florian <Florian@FBPC.fritz.box>
2018-02-15 22:50:10 +0100

    Added IncomingMessagesHandler zum Unterscheiden von gesendeten Objekten bzw. JSON-Strings, wieder nur in der Testklasse TODO Design des Programms überdenken in Bezug auf Unterscheidung von JSON-Messages Added Basic Apache Shiro Struktur für Login-Prozedur...nicht funktionsfähig Added @ResultSetMapping Added @NamedNativeQuery um SQLResultSets in Entities zu transformieren, funktioniert allerdings bis jetzt nur in der CRUD-Testklasse TODO Alles in das Programm einarbeiten TODO Work on Database_Controller NextTODO Implement method to extract POJO from Database NextBigTodo Implement Apache Shiro on Server / alle Shiro relevanten methoden auf dem Server siehe http://shiro-user.582556.n2.nabble.com/How-should-we-go-about-configuring-a-Desktop-Client-with-Shiro-in-the-Server-td7581322.html

--- Anzeigeprojekt_Server/src/main/java/com/flopewsserver/controller/Security_Controller.java
+++ Anzeigeprojekt_Server/src/main/java/com/flopewsserver/controller/Security_Controller.java
@@ -5,6 +5,17 @@
  */
 package com.flopewsserver.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
 /**
  *
  * @author Florian
@@ -13,11 +24,55 @@ public class Security_Controller {
    
 public Boolean logincheck(){
 
-    Boolean userexist = false;
+     // The easiest way to create a Shiro SecurityManager with configured
+        // realms, users, roles and permissions is to use the simple INI config.
+        // We'll do that by using a factory that can ingest a .ini file and
+        // return a SecurityManager instance:
+
+        // Use the shiro.ini file at the root of the classpath
+        // (file: and url: prefixes load from files and urls respectively):
+    
+    Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+    org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+    
+     // for this simple example quickstart, make the SecurityManager
+        // accessible as a JVM singleton.  Most applications wouldn't do this
+        // and instead rely on their container configuration or web.xml for
+        // webapps.  That is outside the scope of this simple quickstart, so
+        // we'll just do the bare minimum so you can continue to get a feel
+        // for things.
+        
+     // to use Shiro you always need a subject, which represents the current user, who is unknown at the time of instantiation und has to be filled with 
+     // attributes e.g. from a database or a login screen
+        
+     SecurityUtils.setSecurityManager(securityManager);
+     Subject currentUser = SecurityUtils.getSubject();
+     
+    Session session = currentUser.getSession();
+    session.setAttribute( "someKey", "aValue" );
+     
+        // let's login the current user so we can check against roles and permissions:
+        if (!currentUser.isAuthenticated()) {
+    //im folgenden müssen die Daten des aktuellen Subjects z.B. aus einer Eingabemaske gesammelt werden und mit dem Subjekt in der Datenbank verglichen werden....        
+               UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
+            token.setRememberMe(true);
+            try {
+                currentUser.login(token);
+            } catch (UnknownAccountException uae) {
+                log.info("There is no user with username of " + token.getPrincipal());
+            } catch (IncorrectCredentialsException ice) {
+                log.info("Password for account " + token.getPrincipal() + " was incorrect!");
+            } catch (LockedAccountException lae) {
+                log.info("The account for username " + token.getPrincipal() + " is locked.  " +
+                        "Please contact your administrator to unlock it.");
+            }
+            // ... catch more exceptions here (maybe custom ones specific to your application?
+            catch (AuthenticationException ae) {
+                //unexpected condition?  error?
+            }
+        }
     
     
-    
-    return userexist;
     
 
 
